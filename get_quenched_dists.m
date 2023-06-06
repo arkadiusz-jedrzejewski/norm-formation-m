cec86b8d09bed4219e77965dfeb1c723eda10634
Arkadiusz <arekjd@gmail.com>
2023-06-06 10:01:09 +0200

    distibutions changed to column vector

--- get_quenched_dists.m
+++ get_quenched_dists.m
@@ -2,9 +2,9 @@ function [dist_a,dist_a0,dist_a1] = get_quenched_dists(dist,N0,N1)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 N = N0 + N1;
-dist_a = zeros(1, N+1);
-dist_a0 = zeros(1, N0+1);
-dist_a1 = zeros(1, N1+1);
+dist_a = zeros(N+1, 1);
+dist_a0 = zeros(N0+1, 1);
+dist_a1 = zeros(N1+1, 1);
 
 for i=1:N0+1
     for j=1:N1+1
