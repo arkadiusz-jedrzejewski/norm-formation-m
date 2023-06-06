94906069dab1f77f3539fac963852af9023b52b6
Arkadiusz <arekjd@gmail.com>
2023-06-06 07:38:41 +0200

    changed parapeters

--- dist_evol_quenched.m
+++ dist_evol_quenched.m
@@ -1,13 +1,13 @@
 clearvars; clc; close all;
 
 % model parameters
-N0 = 90;    % number of social learners
-N1 = 10;    % number of individual learners
+N0 = 45;    % number of social learners
+N1 = 15;    % number of individual learners
 N = N0+N1;  % number of all learners
 k = -15;
 q = 3;
 s = 0;
-T = 500; % time horizon in Monte Carlo steps (1 MCS = N updates of the system)
+T = 4000; % time horizon in Monte Carlo steps (1 MCS = N updates of the system)
 
 % time horizon in updates
 t = N*T;
@@ -26,8 +26,7 @@ transition_matrix = get_tran_m_quenched(q, k, s, N0, N1);
 
 % evolution of probability distibution
 dist = transition_matrix^t*dist;
-
-[dist_a, dist_a0, dist_a1] = get_quenched_dists(dist,N0,N1);
+[dist_a, dist_a0, dist_a1] = get_quenched_dists(dist, N0, N1);
 
 plot(a, dist_a, ':.')   % dist for all learners
 hold on
@@ -45,7 +44,7 @@ dist = zeros((N0+1)*(N1+1),1);
 dist(1) = 1;    % initial state
 for t=1:T
     dist = transition_matrix^N*dist;
-    [dist_a,dist_a0,dist_a1] = get_quenched_dists(dist,N0,N1);
+    [dist_a,dist_a0,dist_a1] = get_quenched_dists(dist, N0, N1);
 
     plot(a, dist_a, ':.')   % dist for all learners
     hold on
