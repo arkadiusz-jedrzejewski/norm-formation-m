15e93fdb5de439ba61d903d30defee403f3b46c0
Arkadiusz <arekjd@gmail.com>
2023-06-06 09:41:29 +0200

    clearing disitibution of state corrected

--- finite_phase_diagram_annealed.m
+++ finite_phase_diagram_annealed.m
@@ -5,14 +5,10 @@ N = 500;
 s = 0;
 q = 7;
 k = -15;
-T = 1000000; % time horizon in Monte Carlo steps (1 MCS = N updates of the system)
+T = 10000; % time horizon in Monte Carlo steps (1 MCS = N updates of the system)
 
 % possible states
 a = (0:N)/N;
-
-% probability distibution of states
-dist = zeros(N+1,1);
-dist(1) = 1;    % initial state
 
 % time horizon in updates
 t = N*T;
@@ -21,10 +17,15 @@ P = 0:0.001:0.3;
 a_mean = zeros(1,length(P));
 
 for i=1:length(P)
-    p = P(i)
+    p = P(end+1-i)
+
+    % probability distibution of states
+    dist = zeros(N+1,1);
+    dist(N/2) = 1;    % initial state
+
     % transition matrix
     transition_matrix = get_tran_m_annealed(p, q, k, s, N);
-    
+   
     % evolution of probability distibution
     dist = transition_matrix^t*dist;
     a_mean(i) = max(a, 1-a)*dist;
