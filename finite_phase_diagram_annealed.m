b6159fe2325fc133539e5d30c56d52b77cea2e5a
Arkadiusz <arekjd@gmail.com>
2023-06-07 23:15:04 +0200

    changed parametes

--- finite_phase_diagram_annealed.m
+++ finite_phase_diagram_annealed.m
@@ -1,13 +1,18 @@
-clearvars; clc;% close all;
+clearvars; clc; %close all;
 
 % model parameters
-N = 500;
+N = 3200;
 s = 0;
-q = 7;
-k = -15;
+q = 2;
+k = 0;
 T = 10000; % time horizon in Monte Carlo steps (1 MCS = N updates of the system)
-P = 0:0.001:0.3;
+P = linspace(0,0.4,80);
 
 a_mean = get_finite_diagram(P,s,q,k,N,T);
 
 plot(P, a_mean, '.:')
+hold on
+% 
+% a = 0:0.01:1;
+% p = get_fixed_points(a,q,k,s,true);
+% plot(p, a, 'k')
