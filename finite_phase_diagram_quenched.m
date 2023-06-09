e8df5b9892315a4925c425a2436756d592944ce6
Arkadiusz <arekjd@gmail.com>
2023-06-09 23:00:04 +0200

    function get_fixed points extended by one arguemnt

--- finite_phase_diagram_quenched.m
+++ finite_phase_diagram_quenched.m
@@ -1,8 +1,8 @@
 clearvars; clc; close all;
 
 % model parameters
-N = 20;  % number of all learners
-k = -15;
+N = 50;  % number of all learners
+k = -45;
 q = 3;
 s = 0;
 T = 40000; % time horizon in Monte Carlo steps (1 MCS = N updates of the system)
@@ -15,7 +15,7 @@ plot(P, a0_mean, '.:')
 plot(P, a1_mean, '.:')
 
 a = 0:0.01:1;
-p = get_fixed_points(a,q,k,s,false);
+p = get_fixed_points(a,q,k,s,false,false);
 plot(p, a, 'k')
 
 xlabel('p')
