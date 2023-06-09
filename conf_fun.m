d4005abf7cf27d82cf9946a08fcb55ae1bd29c6f
Arkadiusz <arekjd@gmail.com>
2023-06-09 13:40:20 +0200

    added symmetric option

--- conf_fun.m
+++ conf_fun.m
@@ -1,5 +1,10 @@
-function [prob] = conf_fun(x,q)
+function [prob] = conf_fun(x,q,is_symmetric)
 % conformity function representing social learning
-% non-symmetric conformity function - the nonlinear q-voter modelconf_
-prob = x.^q;
+% non-symmetric conformity function - the nonlinear q-voter model
+if is_symmetric
+    prob = 0.5*(2.*x).^q;
+    prob(x>=0.5)=1-0.5.*(2.*(1-x(x>=0.5))).^q;
+else
+    prob = x.^q;
+end
 end
