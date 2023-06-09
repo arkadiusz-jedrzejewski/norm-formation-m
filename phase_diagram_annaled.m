567f3c8a437f8838b812b51d41f5dc99619d3788
Arkadiusz <arekjd@gmail.com>
2023-06-09 22:57:13 +0200

    function get_fixed_points extended by one argument

--- phase_diagram_annaled.m
+++ phase_diagram_annaled.m
@@ -1,13 +1,13 @@
 clearvars; clc; close all;
 
 a=0:0.001:1;
-K=3.9;
+K=-20:0.5:4;
 s=0;
-q=20;
+q=2;
 %annealed model
 for i=1:length(K)
     k=K(i);
-    [p] = get_fixed_points(a,q,k,s,false);
+    [p] = get_fixed_points(a,q,k,s,true,true);
 
     plot(p,a)
     title(['q=' num2str(q) ' k=' num2str(k)])
