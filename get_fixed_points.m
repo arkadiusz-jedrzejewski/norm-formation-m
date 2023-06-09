4b9f1b08c598611f201832dc449d597f30df6392
Arkadiusz <arekjd@gmail.com>
2023-06-09 13:42:13 +0200

    added symmetric option

--- get_fixed_points.m
+++ get_fixed_points.m
@@ -1,4 +1,4 @@
-function [p] = get_fixed_points(a,q,k,s,is_annealed)
+function [p] = get_fixed_points(a,q,k,s,is_annealed,is_symmetric)
 % returns fixed points of the model (p, a)
 % annealed mode: valid for any ditribution of inclinations towards
 % individual learning (p is the mean of the distibution)
@@ -7,11 +7,11 @@ function [p] = get_fixed_points(a,q,k,s,is_annealed)
 % distibutions, fixed points can be carculated as it is presented in the
 % manuscript)
 
-p_num = s*(a-1/2)+(1-s)*(a.*(conf_fun(a,q)+conf_fun(1-a,q))-conf_fun(a,q));
+p_num = s*(a-1/2)+(1-s)*(a.*(conf_fun(a,q,is_symmetric)+conf_fun(1-a,q,is_symmetric))-conf_fun(a,q,is_symmetric));
 if is_annealed
-    p_den = (1-s)*(a.*(conf_fun(1-a,q)+conf_fun(a,q)-1)-conf_fun(a,q)+indiv_fun(a,k));
+    p_den = (1-s)*(a.*(conf_fun(1-a,q,is_symmetric)+conf_fun(a,q,is_symmetric)-1)-conf_fun(a,q,is_symmetric)+indiv_fun(a,k));
 else
-    p_den = (s/2+(1-s)*indiv_fun(a,k)).*(s+(1-s)*(conf_fun(a,q)+conf_fun(1-a,q)))-s/2-(1-s)*conf_fun(a,q);
+    p_den = (s/2+(1-s)*indiv_fun(a,k)).*(s+(1-s)*(conf_fun(a,q,is_symmetric)+conf_fun(1-a,q,is_symmetric)))-s/2-(1-s)*conf_fun(a,q,is_symmetric);
 end
 p = p_num./p_den;
 end
