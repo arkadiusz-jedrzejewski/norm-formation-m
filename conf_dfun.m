0205b85fc0f3782a6b25e4f36662870e69ab0d79
Arkadiusz <arekjd@gmail.com>
2023-06-11 19:40:18 +0200

    symmetric and non-symmetric swapped - error corredted

--- conf_dfun.m
+++ conf_dfun.m
@@ -5,12 +5,6 @@ if is_symmetric
     if q==0
         dp = 0;
     else
-        dp = q*x^(q-1);
-    end
-else
-    if q==0
-        dp = 0;
-    else
         if (0.5>x) && (x>=0)
             dp = q*(2*x)^(q-1);
         elseif (0.5<=x) && (x<=1)
@@ -18,6 +12,12 @@ else
         else
             dp = 0;
         end
+    end
+else
+    if q==0
+        dp = 0;
+    else
+        dp = q*x^(q-1);
     end
 end
 end
