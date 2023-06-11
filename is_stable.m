494423b7d6e69430f7428a05b7e96126eafc03ae
Arkadiusz <arekjd@gmail.com>
2023-06-11 18:03:36 +0200

    added chacking atability for quenched model

--- is_stable.m
+++ is_stable.m
@@ -7,6 +7,16 @@ if is_annealed
 else
     % quenched case (Bernoulli disitribution if conformity function is
     % non-symmetric)
-    stable = false;
+    a0 = (s/2+(1-s)*conf_fun(a,q,is_symmetric))/(s+(1-s)*(conf_fun(a,q,is_symmetric)+conf_fun(1-a,q,is_symmetric)));
+
+    F00 = -s-(1-s)*conf_fun(a,q,is_symmetric)+(1-a0)*(1-s)*conf_dfun(a,q,is_symmetric)*(1-p)-...
+        (1-s)*conf_fun(1-a,q,is_symmetric)+a0*(1-s)*conf_dfun(1-a)*(1-p);
+    F01 = (1-a0)*(1-s)*conf_dfun(a,q,is_symmetric)*p+a0*(1-s)*conf_dfun(1-a)*p;
+    F11 = (1-s)*indiv_dfun(a,k)*p-1;
+    F10 = (1-s)*indiv_dfun(a,k)*(1-p);
+
+    det = F00*F11-F01*F10;
+    tra = F00 + F11;
+    stable = (det>0) && (tra<0);
 end
 end
