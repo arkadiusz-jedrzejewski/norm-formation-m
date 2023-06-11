6ea0bcfb6a059c50db885168d6511281ed02769c
Arkadiusz <arekjd@gmail.com>
2023-06-12 00:39:57 +0200

    function returning ciritcal point added

---
+++ get_critical_point.m
@@ -0,0 +1,12 @@
+function [pc] = get_critical_point(q,k,s,is_annealed,is_symmetric)
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+if is_symmetric
+    pc = 4*(q-1-q*s)/((1-s)*(4*q-k));
+else
+    if is_annealed
+        pc = (q-1-s*(q-1+2^(q-1)))/((1-s)*(q-1+2^(q-1)*(1-k/4)));
+    else
+        pc = (4*(q-1)-4*s*(q-1+2^(q-1)))/((1-s)*(4*q-k+k*s*(1-2^(q-1))));
+    end
+end
