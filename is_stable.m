d8b79ac51050ab61f70256b42334eeee66e5de6e
Arkadiusz <arekjd@gmail.com>
2023-06-11 18:46:30 +0200

    missing arguments added

--- is_stable.m
+++ is_stable.m
@@ -13,8 +13,8 @@ else
     a0 = (s/2+(1-s)*conf_fun(a,q,is_symmetric))/(s+(1-s)*(conf_fun(a,q,is_symmetric)+conf_fun(1-a,q,is_symmetric)));
 
     F00 = -s-(1-s)*conf_fun(a,q,is_symmetric)+(1-a0)*(1-s)*conf_dfun(a,q,is_symmetric)*(1-p)-...
-        (1-s)*conf_fun(1-a,q,is_symmetric)+a0*(1-s)*conf_dfun(1-a)*(1-p);
-    F01 = (1-a0)*(1-s)*conf_dfun(a,q,is_symmetric)*p+a0*(1-s)*conf_dfun(1-a)*p;
+        (1-s)*conf_fun(1-a,q,is_symmetric)+a0*(1-s)*conf_dfun(1-a,q,is_symmetric)*(1-p);
+    F01 = (1-a0)*(1-s)*conf_dfun(a,q,is_symmetric)*p+a0*(1-s)*conf_dfun(1-a,q,is_symmetric)*p;
     F11 = (1-s)*indiv_dfun(a,k)*p-1;
     F10 = (1-s)*indiv_dfun(a,k)*(1-p);
 
