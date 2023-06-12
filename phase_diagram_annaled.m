bbfbd373e94e49c8850b25baad646bf0604ace16
Arkadiusz <arekjd@gmail.com>
2023-06-12 17:13:49 +0200

    parameter chenged

--- phase_diagram_annaled.m
+++ phase_diagram_annaled.m
@@ -1,9 +1,9 @@
 clearvars; clc; close all;
 
 a=0:0.001:1;
-K=-20:0.5:4;
+K=-40:0.1:-10;
 s=0;
-q=2;
+q=8;
 %annealed model
 for i=1:length(K)
     k=K(i);
@@ -17,7 +17,7 @@ for i=1:length(K)
     hold on
     plot(pu,a,'r')
     title(['q=' num2str(q) ' k=' num2str(k)])
-    xlim([0 1])
+    xlim([0 0.8])
     xlabel('p')
     ylabel('a^*')
     hold off
