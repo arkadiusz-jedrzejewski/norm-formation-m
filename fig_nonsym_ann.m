1038dd07d4649038ee560423c57fda1799c514eb
Arkadiusz <arekjd@gmail.com>
2023-06-12 10:27:33 +0200

    plotting phase space for article

---
+++ fig_nonsym_ann.m
@@ -0,0 +1,54 @@
+clearvars; clc; close all;
+
+a=0:0.0001:1;
+s=0;
+q=4;
+
+subplot(1,4,1)
+k=-15;
+plot_diagram(a,q,k,s,true,false)
+xlim([0, 0.2])
+xlabel('p')
+ylabel('a^*')
+title('k<k^*(q)')
+
+plot([0, 0.2], [0.5, 0.5], 'k:')
+
+subplot(1,4,2)
+k=-7.077;
+plot_diagram(a,q,k,s,true,false)
+xlim([0, 0.2])
+xlabel('p')
+ylabel('a^*')
+title('k=k^*(q)')
+
+
+subplot(1,4,3)
+k=-4;
+plot_diagram(a,q,k,s,true,false)
+xlim([0, 0.2])
+xlabel('p')
+ylabel('a^*')
+title('k>k^*(q)')
+
+subplot(1,4,4)
+k=-15:0.001:4;
+q = -(2.^(1/2).*(-(k - 4).*(k.^3 - 50.*k + 200)).^(1/2) - 10*k + 40)./(4.*(k - 4));
+
+tol = 1e-20;
+idx = abs(imag(q))<tol;
+plot(q(idx), k(idx),'k')
+hold on
+
+q = (10*k + 2^(1/2)*(-(k - 4).*(k.^3 - 50*k + 200)).^(1/2) - 40)./(4*(k - 4));
+
+tol = 1e-10;
+idx = abs(imag(q))<tol;
+plot(q(idx), k(idx),'k')
+
+xlim([1,8])
+xlabel('q')
+ylabel('k^*(q)')
+
+hold on
+plot([0, 8], [0, 0], 'k:')
---
+++ fig_nonsym_que.m
@@ -0,0 +1,43 @@
+clearvars; clc; close all;
+
+a=0:0.0001:1;
+s=0;
+q=4;
+
+subplot(1,4,1)
+k=-30;
+plot_diagram(a,q,k,s,false,false)
+xlim([0, 0.6])
+xlabel('p')
+ylabel('a^*')
+title('k<k^*(q)')
+
+plot([0, 0.6], [0.5, 0.5], 'k:')
+
+subplot(1,4,2)
+k=-19.63;
+plot_diagram(a,q,k,s,false,false)
+xlim([0, 0.6])
+xlabel('p')
+ylabel('a^*')
+title('k=k^*(q)')
+
+
+subplot(1,4,3)
+k=-10;
+plot_diagram(a,q,k,s,false,false)
+xlim([0, 0.6])
+xlabel('p')
+ylabel('a^*')
+title('k>k^*(q)')
+
+subplot(1,4,4)
+k=-50:0.01:4;
+q = -(2*k + ((k - 2).*(k - 4).*(k.^2 + 2*k + 8)).^(1/2) - 8)./(4*(k - 4));
+
+plot(q, k,'k')
+
+
+xlim([1,8])
+xlabel('q')
+ylabel('k^*(q)')
