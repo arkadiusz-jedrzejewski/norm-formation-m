ff9bcfc016f32140e8b14c1fd0ea7e0a12caad0b
Arkadiusz <arekjd@gmail.com>
2023-06-15 13:54:54 +0200

    non-symmetric options added

--- k_star.m
+++ k_star.m
@@ -1,7 +1,7 @@
-function error = k_star(q,k)
+function error = k_star(q,k,is_annealed,is_symmetric)
 % function used for finding a line separating continuous and discontinuous
 % phase transitions
-    a_infl = max(get_roots(@(x)ddp_symmetric(x,q,k),0,0.5,0.001,1e-12));
+    a_infl = max(get_roots(@(x)ddp_symmetric(x,q,k,is_annealed,is_symmetric),0,0.5,0.001,1e-12));
     %display(a_infl)
-    error = abs(dp_symmetric(a_infl,q,k));
+    error = abs(dp_symmetric(a_infl,q,k,is_annealed,is_symmetric));
 end
