e7bbd9a585e08e5f15c66c2b4d47296fb2a02d0f
Arkadiusz <arekjd@gmail.com>
2023-06-15 15:17:20 +0200

    case when no root is found added

--- k_star.m
+++ k_star.m
@@ -2,6 +2,9 @@ function error = k_star(q,k,is_annealed,is_symmetric)
 % function used for finding a line separating continuous and discontinuous
 % phase transitions
     a_infl = max(get_roots(@(x)ddp_symmetric(x,q,k,is_annealed,is_symmetric),0,0.5,0.001,1e-12));
-    %display(a_infl)
-    error = abs(dp_symmetric(a_infl,q,k,is_annealed,is_symmetric));
+    if isempty(a_infl)
+        error = inf;
+    else
+        error = abs(dp_symmetric(a_infl,q,k,is_annealed,is_symmetric));
+    end
 end
