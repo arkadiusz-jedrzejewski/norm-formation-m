32a613cf06fd6183b14bc49dc19d55ac3ac1688b
Arkadiusz <arekjd@gmail.com>
2023-06-15 14:10:18 +0200

    typo corrected

--- distance_p.m
+++ distance_p.m
@@ -1,7 +1,7 @@
-function diff = distance_p(q,k, is_annealed, is_symmetic)
+function diff = distance_p(q,k, is_annealed, is_symmetric)
 % function used for finding the line for thich the upper border of
 % matastable reagion coincides with the critical point
-    tab_a = get_roots(@(x)dp_symmetric(x,q,k, is_annealed, is_symmetic),0,0.5,0.001,1e-12);
+    tab_a = get_roots(@(x)dp_symmetric(x,q,k, is_annealed, is_symmetric),0,0.5,0.001,1e-12);
     tab_p = get_fixed_points(tab_a,q,k,0,is_annealed,is_symmetric);
     if isempty(tab_p)
         diff = 0;
