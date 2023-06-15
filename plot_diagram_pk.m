fadf7ced87f3590837010f224a1d53df95bd0830
Arkadiusz <arekjd@gmail.com>
2023-06-15 15:10:37 +0200

    inflection line added

--- plot_diagram_pk.m
+++ plot_diagram_pk.m
@@ -1,11 +1,12 @@
-function [] = plot_diagram_pk(K, q)
+function [] = plot_diagram_pk(K, q, is_annealed, is_symmetric)
     p_up = zeros(1,length(K));
     p_low = zeros(1,length(K));
-    p_inf = zeros(1,length(K));
+    p_inf_up = zeros(1,length(K));
+    p_inf_low = zeros(1,length(K));
     for i=1:length(K)
-        tab = conf_fun_sym(get_roots(@(x)dp_symmetric(x,q,K(i)),0,0.5,0.001,1e-12),q,K(i));
-        tab2_a = get_roots(@(x)ddp_symmetric(x,q,K(i)),0,0.5,0.001,1e-12);
-        tab2 = conf_fun_sym(tab2_a,q,K(i));
+        tab = get_fixed_points(get_roots(@(x)dp_symmetric(x,q,K(i), is_annealed, is_symmetric),0,0.5,0.003,1e-12),q,K(i),0 ,is_annealed, is_symmetric);
+        tab2_a = get_roots(@(x)ddp_symmetric(x,q,K(i), is_annealed, is_symmetric),0,0.5,0.003,1e-12);
+        tab2 = get_fixed_points(tab2_a,q,K(i), 0,is_annealed, is_symmetric);
         if isempty(tab)
             p_up(i) = nan;
             p_low(i) = nan;
@@ -14,29 +15,34 @@ function [] = plot_diagram_pk(K, q)
             p_low(i) = min(tab);
         end
         if isempty(tab2)
-            p_inf(i)=nan;
+            p_inf_up(i)=nan;
+            p_inf_low(i)=nan;
             %p_c(i)=nan;
         else
-            [val,it]=max(tab2);
-            p_inf(i)=val;
+            p_inf_up(i)=max(tab2);
+            p_inf_low(i)=min(tab2);
             %p_c(i) = tab2_a(it);
         end
     end
     plot(K,p_up)
     hold on
     plot(K,p_low)
-    plot(K,4*(q-1)./(4*q-K),'k')
+    pc = get_critical_point(q,K,0,is_annealed,is_symmetric);
+    plot(K,pc,'k')
     xlabel('k')
     ylabel('p')
     title(['q=' num2str(q)])
+    plot(K,p_inf_low,'c')
+    plot(K,p_inf_up,'c')
 
-    k_st=fminsearch(@(x)k_star(q,x),-25);
-    c_infl=max(get_roots(@(x)ddp_symmetric(x,q,k_st),0,0.5,0.001,1e-12));
-    p_infl = conf_fun_sym(c_infl,q,k_st);
+    k_st=fminsearch(@(x)k_star(q,x, is_annealed, is_symmetric),3.5);
+    c_infl=max(get_roots(@(x)ddp_symmetric(x,q,k_st, is_annealed, is_symmetric),0,0.5,0.001,1e-12));
+    p_infl = get_fixed_points(c_infl,q,k_st,0,is_annealed, is_symmetric);
     plot(k_st,p_infl,'.r')
     
-    k_t=fminsearch(@(x)distance_p(q,x),-32);
-    p_t= conf_fun_sym(get_roots(@(x)dp_symmetric(x,q,k_t),0,0.5,0.001,1e-12),q,k_t);
+
+    k_t=fminsearch(@(x)distance_p(q,x, is_annealed, is_symmetric),3.5);
+    p_t= get_fixed_points(get_roots(@(x)dp_symmetric(x,q,k_t, is_annealed, is_symmetric),0,0.5,0.001,1e-12),q,k_t,0,is_annealed, is_symmetric);
     plot(k_t,p_t,'.k')
 %     for i=1:length(p_c)
 %         dp_s(i)=dp_symmetric(p_c(i),q,K(i));
