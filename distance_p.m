90ab0f1877cb8502a40ffc1bf18f8eaa65eb69d7
Arkadiusz <arekjd@gmail.com>
2023-06-15 14:04:30 +0200

    non-symmetric options added

--- distance_p.m
+++ distance_p.m
@@ -1,8 +1,8 @@
-function diff = distance_p(q,k)
+function diff = distance_p(q,k, is_annealed, is_symmetic)
 % function used for finding the line for thich the upper border of
 % matastable reagion coincides with the critical point
-    tab_a = get_roots(@(x)dp_symmetric(x,q,k),0,0.5,0.001,1e-12);
-    tab_p = conf_fun_sym(tab_a,q,k);
+    tab_a = get_roots(@(x)dp_symmetric(x,q,k, is_annealed, is_symmetic),0,0.5,0.001,1e-12);
+    tab_p = get_fixed_points(tab_a,q,k,0,is_annealed,is_symmetric);
     if isempty(tab_p)
         diff = 0;
     else
