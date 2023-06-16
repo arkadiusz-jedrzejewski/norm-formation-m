d2177989a2e40f1e29563508cd5b2821f10d5f5f
Arkadiusz <arekjd@gmail.com>
2023-06-16 09:04:05 +0200

    added disorder and confomrity fucntion type option

--- plot_diagram_kq.m
+++ plot_diagram_kq.m
@@ -1,10 +1,20 @@
-function [] = plot_diagram_kq(Q)
+function [] = plot_diagram_kq(Q,is_annealed,is_symmetric)
     Kt = zeros(1,length(Q));
     Kst = zeros(1,length(Q));
-    
     for i=1:length(Q)
-        Kt(i)=fminsearch(@(x)distance_p(Q(i),x,true,true),-32);
-        Kst(i)=fminsearch(@(x)k_star(Q(i),x,true,true),-25);
+        [kt,fval] = fminsearch(@(x)distance_p(Q(i),x,is_annealed,is_symmetric),-25);
+        if fval < 1e-4
+            Kt(i) = kt;
+        else
+            Kt(i) = nan;
+        end
+
+        [kst,fval] = fminsearch(@(x)k_star(Q(i),x,is_annealed,is_symmetric),-25);
+        if fval < 1e-4
+            Kst(i) = kst;
+        else
+            Kst(i) = nan;
+        end
     end
     plot(Q,Kt,'.:')
     hold on
